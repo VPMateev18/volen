e3938a3811d1086b7cfd731c5a47f90638aeb021
VPMateev18 <58166499+VPMateev18@users.noreply.github.com>
2022-12-01 08:52:00 +0200

    Add files via upload

---
+++ volen-master/src/test/java/com/example/wfouh/WfouhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wfouh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfouhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
